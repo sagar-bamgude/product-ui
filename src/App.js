9b833324f5b261ad2c682de28cab4b5fef78d7e8
sagar-bamgude <sbamgude1@gmail.com>
2025-01-30 15:26:00 +0530

    remove build and code Ql error

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ const App = () => {
   const fetchProducts = async () => {
     const response = await axios.get('http://localhost:5000/products');
     setProducts(response.data);
+    setCart([])
   };
 
   const handleLogin = async () => {
