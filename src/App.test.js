eb7420f853c3caebb7d70f9fa4e0433d4e928244
sagar-bamgude <sbamgude1@gmail.com>
2025-01-30 15:18:19 +0530

    resolve build error

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import App from './App';
+import App from './App.js';
 
 test('renders learn react link', () => {
   render(<App />);
